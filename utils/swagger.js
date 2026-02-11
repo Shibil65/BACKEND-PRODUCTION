2ea93026ded5897785f481cfba62311d6a8f16ab
shibil <shibilkp6657@gmail.com>
2026-02-11 18:11:16 +0530

    Add: SWAGGER API DOCUMENTATION and JWT Authentication

---
+++ utils/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Task Management API",
+      version: "1.0.0",
+      description: "Machine Test Backend API Documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["../routes/*.js"], // where swagger comments exist
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, specs };
